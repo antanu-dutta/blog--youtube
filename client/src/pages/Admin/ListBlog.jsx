f32e6663b9fdfef94f4b8535750a77a3eb4a4ea7
Antanu Dutta <antanu.bittu@gmail.com>
2025-09-30 13:29:21 +0530

    second commit

--- client/src/pages/Admin/ListBlog.jsx
+++ client/src/pages/Admin/ListBlog.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { blog_data } from "../../assets/assets.js";
+import BlogTableItem from "../../components/Admin/BlogTableItem.jsx";
 
 const ListBlog = () => {
-  return <div>ListBlog</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <h1>All Blogs</h1>
+
+      <div className="relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-600 text-left uppercase">
+            <tr>
+              <th scope="col" className="px-2 py-4 xl:px-6">
+                #
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Blog Title
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm-hidden">
+                Date
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm-hidden">
+                Status
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs.map((blog, index) => (
+              <BlogTableItem
+                key={index}
+                blog={blog}
+                index={index + 1}
+                fetchBlogs={fetchBlogs}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ListBlog;
